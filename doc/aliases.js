6b522328bf0e8271cfd1e5a972bf1c6864b6e4f6
Dan Aloni <alonid@gmail.com>
2019-01-21 10:46:01 +0200

    Update gh-pages to v0.2.0-10-gea71e37122f6

--- doc/aliases.js
+++ doc/aliases.js
@@ -1,7 +1,10 @@
 var ALIASES = {};
 ALIASES['interact'] = {};
 ALIASES['interact'] = {};
+ALIASES['interact'] = {};
 ALIASES['interact_derive'] = {};
 ALIASES['interact_derive'] = {};
+ALIASES['interact_derive'] = {};
+ALIASES['interact_prompt'] = {};
 ALIASES['interact_prompt'] = {};
 ALIASES['interact_prompt'] = {};
